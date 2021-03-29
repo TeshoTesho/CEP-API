22e9fcea3d8db8a92d6d25ec4135ae237da39cad
Nicolas Leite araujo <72231367+TeshoTesho@users.noreply.github.com>
2021-03-29 17:09:44 -0300

    cep-json
    Atalizando com:
    Máscaras e funções a fins.

--- cep_json.php
+++ cep_json.php
@@ -2,15 +2,14 @@
 //JSON
 //Func buscar endereço atraves do cep
 function get_endereco_cep($cep_pesquisa){
-
+	$cep_pesquisa = preg_replace( '/[^0-9]/is', '', $cep_pesquisa ); // Limpa string
   //Permissão ssl para acesso
-    $arrContextOptions=array(
-      "ssl"=>array(
-          "verify_peer"=>false,
-          "verify_peer_name"=>false,
-      ),
-  );  
-  
+  $arrContextOptions=array(
+    "ssl"=>array(
+      "verify_peer"=>false,
+      "verify_peer_name"=>false,
+    ),
+  );    
   // formatar o cep removendo caracteres nao numericos
   $cep_pesquisa = preg_replace("/[^0-9]/", "", $cep_pesquisa);
   //Url de consulta XML
@@ -19,15 +18,62 @@ function get_endereco_cep($cep_pesquisa){
   return $resultado_cep;
 }
 
-//CEP UTILIZADO 
-$endereço = get_endereco_cep("11701390");
 
-echo "<br>CEP: " . $endereço->cep;
-echo "<br>Logradouro: " . $endereço->logradouro;
-echo "<br>Complemento: " . $endereço->complemento;
-echo "<br>Bairro: " . $endereço->bairro;
-echo "<br>Cidade: " . $endereço->localidade;
-echo "<br>UF: " . $endereço->uf;
-echo "<hr><h2>Json:</h2>";
-print_r($endereço);
+function mask($val){
+  $mask = "#####-###";
+  $val = preg_replace( '/[^0-9]/is', '', $val);
+  $maskared = '';
+  $k = 0;
+  for ($i = 0; $i <= strlen($mask) - 1; ++$i) {
+    if ($mask[$i] == '#') {
+      if (isset($val[$k])) {
+        $maskared .= $val[$k++];
+      }
+    } else {
+      if (isset($mask[$i])) {
+        $maskared .= $mask[$i];
+      }
+    }
+  }
+  return $maskared;
+}
+
+function endereço($cep){
+	$endereço = @get_endereco_cep($cep);
+	return $endereço==false ? false : $endereço->logradouro;
+}
+function complemento($cep){
+	$endereço =@get_endereco_cep($cep);
+	return $endereço==false ? false : $endereço->complemento;
+}
+function bairro($cep){
+	$endereço =@get_endereco_cep($cep);
+	return $endereço==false ? false : $endereço->bairro;
+}
+function cidade($cep){
+	$endereço =@get_endereco_cep($cep);
+	return $endereço==false ? false : $endereço->localidade;
+}
+function uf($cep){
+	$endereço =@get_endereco_cep($cep);
+	return $endereço==false ? false : $endereço->uf;
+}
+function json($cep){
+	$endereço =@get_endereco_cep($cep);
+	return $endereço==false ? false : print_r($endereço);
+}
+
+
+//CEP UTILIZADO 
+$cep = '11701390';
+$endereço = get_endereco_cep($cep);
+echo $endereço==false ? "" : "<br>CEP: " . mask($cep);
+echo $endereço==false ? "" : "<br>Logradouro: " . $endereço->logradouro;
+echo $endereço==false ? "" : "<br>Complemento: " . $endereço->complemento;
+echo $endereço==false ? "" : "<br>Bairro: " . $endereço->bairro;
+echo $endereço==false ? "" : "<br>Cidade: " . $endereço->localidade;
+echo $endereço==false ? "" : "<br>UF: " . $endereço->uf;
+//echo get_endereco_cep($cep)==false ? "" : "--". json($cep) ."";
+
+
 ?>
